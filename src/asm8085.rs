1eaa4e47f4f111c465883a48ddc59b8faf7135a7
Parth Pant <parthpant4@gmail.com>
2021-10-02 19:50:14 +0530

    make assembler more coherent

--- src/asm8085.rs
+++ src/asm8085.rs
@@ -34,53 +34,34 @@ fn read_file(filename: &str) -> Result<String> {
     Ok(data)
 }
 
-fn get_words(data: &str) -> Vec<Vec<&str>> {
-    let mut res: Vec<Vec<&str>> = Vec::new();
+fn tokenize_words(data: &str) -> Res<Vec<Token>, String> {
+    let mut res: Vec<Token> = Vec::new();
     for line in data.lines() {
-        let mut l: Vec<&str> = Vec::new();
-        for word in line.split_whitespace() {
+        for word in line.to_lowercase().split_whitespace() {
             match word {
                 ";" => break,
-                "," => break, // no need for this here
-                " " => break, 
+                " " => continue, 
                 _ => {
                     if word.chars().next().unwrap() == ';' { // ;Comments :')
                         break;
                     }
-                    l.push(word)
+                    if let Some(token) = tokenize(word) {
+                        res.push(token);
+                    } else {
+                        return Err(format!("Syntax error at {}", word));
+                    }
                 },
             }
         }
-        if l.len() > 0 {
-            res.push(l);
-        }
-    }
-    res
-}
-
-fn lex_line<'a>(line: &'a Vec<&str>) -> Res<Vec<Token>, &'a str> {
-    let mut i = 0;
-    let mut res: Vec<Token> = Vec::new();
-    while i < line.len() {
-        if let Some(t) = tokenize(line[i]) {
-            res.push(t);
-        }
-        i += 1;
     }
     Ok(res)
 }
 
 pub fn assemble(code: &str) -> Res<(Vec<u8>, String), String>{
     let code = code.replace(",", " ");
-    let parsed = get_words(&code);
 
-    let mut tokens: Vec<Token> = Vec::new();
-    for line in parsed.iter() {
-        match lex_line(line) {
-            Ok(mut l) => tokens.append(&mut l),
-            Err(e) => return Err(format!("Syntax error at {}", e)), // this would never happen
-        }
-    }
+    // tokenize the words
+    let tokens: Vec<Token> = tokenize_words(&code)?;
 
     // First Pass: build the symbol table
     let mut symbol_table: HashMap<String, usize> = HashMap::new();
@@ -92,8 +73,6 @@ pub fn assemble(code: &str) -> Res<(Vec<u8>, String), String>{
             _                            => ()
         };
     };
-
-    // println!("{:?}", tokens);
 
     // Second Pass: generate the rom
     let mut bin: Vec<u8> = Vec::new();
@@ -434,117 +413,119 @@ fn tokenize(word: &str) -> Option<Token> { // never returns NONE since this is o
         let temp = word.trim_end_matches(':').to_string();
         return Some(Token::Label(temp));
     }
-    let data = word.clone();
-    let t = match data.as_ref() {
-        //                             Ins   nR nO
-        "aci"  => Some(Token::Mnemonic(data, 0, 1)),   
-        "adc"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "add"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "adi"  => Some(Token::Mnemonic(data, 0, 1)),   
-        "ana"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "ani"  => Some(Token::Mnemonic(data, 0, 1)),   
-        "call" => Some(Token::Mnemonic(data, 0, 2)),   
-        "cc"   => Some(Token::Mnemonic(data, 0, 2)),   
-        "cm"   => Some(Token::Mnemonic(data, 0, 2)),   
-        "cma"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "cmc"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "cmp"  => Some(Token::Mnemonic(data, 1, 2)),   
-        "cnc"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "cnz"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "cp"   => Some(Token::Mnemonic(data, 0, 2)),   
-        "cpe"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "cpi"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "cpo"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "cz"   => Some(Token::Mnemonic(data, 0, 2)),   
-        "daa"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "dcr"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "dcx"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "di"   => Some(Token::Mnemonic(data, 0, 0)),   
-        "ei"   => Some(Token::Mnemonic(data, 0, 0)),   
-        "hlt"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "in"   => Some(Token::Mnemonic(data, 0, 1)),   
-        "inr"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "inx"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "jc"   => Some(Token::Mnemonic(data, 0, 2)),   
-        "jm"   => Some(Token::Mnemonic(data, 0, 2)),   
-        "jmp"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "jnc"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "jnz"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "jp"   => Some(Token::Mnemonic(data, 0, 2)),   
-        "jpe"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "jpo"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "jz"   => Some(Token::Mnemonic(data, 0, 2)),   
-        "lda"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "ldax" => Some(Token::Mnemonic(data, 1, 0)),   
-        "lhld" => Some(Token::Mnemonic(data, 0, 2)),   
-        "lxi"  => Some(Token::Mnemonic(data, 1, 2)),   
-        "mov"  => Some(Token::Mnemonic(data, 2, 0)),   
-        "mvi"  => Some(Token::Mnemonic(data, 1, 1)),   
-        "nop"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "ora"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "ori"  => Some(Token::Mnemonic(data, 0, 1)),   
-        "out"  => Some(Token::Mnemonic(data, 0, 1)),   
-        "pchl" => Some(Token::Mnemonic(data, 0, 0)),   
-        "pop"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "push" => Some(Token::Mnemonic(data, 1, 0)),   
-        "ral"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "rar"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "rc"   => Some(Token::Mnemonic(data, 0, 0)),   
-        "ret"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "rim"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "rlc"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "rm"   => Some(Token::Mnemonic(data, 0, 0)),   
-        "rnc"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "rnz"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "rp"   => Some(Token::Mnemonic(data, 0, 0)),   
-        "rpe"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "rpo"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "rrc"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "rst"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "rz"   => Some(Token::Mnemonic(data, 0, 0)),   
-        "sbb"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "sbi"  => Some(Token::Mnemonic(data, 0, 1)),   
-        "shld" => Some(Token::Mnemonic(data, 0, 2)),   
-        "sim"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "sphl" => Some(Token::Mnemonic(data, 0, 0)),   
-        "sta"  => Some(Token::Mnemonic(data, 0, 2)),   
-        "stax" => Some(Token::Mnemonic(data, 1, 0)),   
-        "stc"  => Some(Token::Mnemonic(data, 0, 0)),   
-        "sub"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "sui"  => Some(Token::Mnemonic(data, 0, 1)),   
-        "xchg" => Some(Token::Mnemonic(data, 0, 0)),   
-        "xra"  => Some(Token::Mnemonic(data, 1, 0)),   
-        "xri"  => Some(Token::Mnemonic(data, 0, 1)),   
-        "xthl" => Some(Token::Mnemonic(data, 0, 0)),   
-        "a"    => Some(Token::Operand(data)),
-        "b"    => Some(Token::Operand(data)),
-        "c"    => Some(Token::Operand(data)),
-        "d"    => Some(Token::Operand(data)),
-        "e"    => Some(Token::Operand(data)),
-        "h"    => Some(Token::Operand(data)),
-        "l"    => Some(Token::Operand(data)),
-        "m"    => Some(Token::Operand(data)),
-        "ab"   => Some(Token::Operand(data)),
-        "bc"   => Some(Token::Operand(data)),
-        "de"   => Some(Token::Operand(data)),
-        "hl"   => Some(Token::Operand(data)),
-        "psw"  => Some(Token::Operand(data)),
-        "sp"   => Some(Token::Operand(data)),
-        "0"    => Some(Token::Operand(data)),
-        "1"    => Some(Token::Operand(data)),
-        "2"    => Some(Token::Operand(data)),
-        "3"    => Some(Token::Operand(data)),
-        "4"    => Some(Token::Operand(data)),
-        "5"    => Some(Token::Operand(data)),
-        "6"    => Some(Token::Operand(data)),
-        "7"    => Some(Token::Operand(data)),
-        _      => Some(Token::Symbol(data)),
-    };
-    if word.contains('h') {
-        let word = word.trim_end_matches('h');
-        if let Ok(word) = i64::from_str_radix(word, 16) {
-            return Some(Token::Data(word as i16));
+
+    if let Some(c) = word.chars().last(){
+        if c == 'h' {
+            let word = word.trim_end_matches('h');
+            if let Ok(word) = i64::from_str_radix(word, 16) {
+                return Some(Token::Data(word as i16));
+            }
         }
     }
-    t
+
+    match word.as_ref() {
+        //                             Ins   nR nO
+        "aci"  => Some(Token::Mnemonic(word, 0, 1)),   
+        "adc"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "add"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "adi"  => Some(Token::Mnemonic(word, 0, 1)),   
+        "ana"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "ani"  => Some(Token::Mnemonic(word, 0, 1)),   
+        "call" => Some(Token::Mnemonic(word, 0, 2)),   
+        "cc"   => Some(Token::Mnemonic(word, 0, 2)),   
+        "cm"   => Some(Token::Mnemonic(word, 0, 2)),   
+        "cma"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "cmc"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "cmp"  => Some(Token::Mnemonic(word, 1, 2)),   
+        "cnc"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "cnz"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "cp"   => Some(Token::Mnemonic(word, 0, 2)),   
+        "cpe"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "cpi"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "cpo"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "cz"   => Some(Token::Mnemonic(word, 0, 2)),   
+        "daa"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "dcr"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "dcx"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "di"   => Some(Token::Mnemonic(word, 0, 0)),   
+        "ei"   => Some(Token::Mnemonic(word, 0, 0)),   
+        "hlt"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "in"   => Some(Token::Mnemonic(word, 0, 1)),   
+        "inr"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "inx"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "jc"   => Some(Token::Mnemonic(word, 0, 2)),   
+        "jm"   => Some(Token::Mnemonic(word, 0, 2)),   
+        "jmp"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "jnc"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "jnz"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "jp"   => Some(Token::Mnemonic(word, 0, 2)),   
+        "jpe"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "jpo"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "jz"   => Some(Token::Mnemonic(word, 0, 2)),   
+        "lda"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "ldax" => Some(Token::Mnemonic(word, 1, 0)),   
+        "lhld" => Some(Token::Mnemonic(word, 0, 2)),   
+        "lxi"  => Some(Token::Mnemonic(word, 1, 2)),   
+        "mov"  => Some(Token::Mnemonic(word, 2, 0)),   
+        "mvi"  => Some(Token::Mnemonic(word, 1, 1)),   
+        "nop"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "ora"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "ori"  => Some(Token::Mnemonic(word, 0, 1)),   
+        "out"  => Some(Token::Mnemonic(word, 0, 1)),   
+        "pchl" => Some(Token::Mnemonic(word, 0, 0)),   
+        "pop"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "push" => Some(Token::Mnemonic(word, 1, 0)),   
+        "ral"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "rar"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "rc"   => Some(Token::Mnemonic(word, 0, 0)),   
+        "ret"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "rim"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "rlc"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "rm"   => Some(Token::Mnemonic(word, 0, 0)),   
+        "rnc"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "rnz"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "rp"   => Some(Token::Mnemonic(word, 0, 0)),   
+        "rpe"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "rpo"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "rrc"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "rst"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "rz"   => Some(Token::Mnemonic(word, 0, 0)),   
+        "sbb"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "sbi"  => Some(Token::Mnemonic(word, 0, 1)),   
+        "shld" => Some(Token::Mnemonic(word, 0, 2)),   
+        "sim"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "sphl" => Some(Token::Mnemonic(word, 0, 0)),   
+        "sta"  => Some(Token::Mnemonic(word, 0, 2)),   
+        "stax" => Some(Token::Mnemonic(word, 1, 0)),   
+        "stc"  => Some(Token::Mnemonic(word, 0, 0)),   
+        "sub"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "sui"  => Some(Token::Mnemonic(word, 0, 1)),   
+        "xchg" => Some(Token::Mnemonic(word, 0, 0)),   
+        "xra"  => Some(Token::Mnemonic(word, 1, 0)),   
+        "xri"  => Some(Token::Mnemonic(word, 0, 1)),   
+        "xthl" => Some(Token::Mnemonic(word, 0, 0)),   
+        "a"    => Some(Token::Operand(word)),
+        "b"    => Some(Token::Operand(word)),
+        "c"    => Some(Token::Operand(word)),
+        "d"    => Some(Token::Operand(word)),
+        "e"    => Some(Token::Operand(word)),
+        "h"    => Some(Token::Operand(word)),
+        "l"    => Some(Token::Operand(word)),
+        "m"    => Some(Token::Operand(word)),
+        "ab"   => Some(Token::Operand(word)),
+        "bc"   => Some(Token::Operand(word)),
+        "de"   => Some(Token::Operand(word)),
+        "hl"   => Some(Token::Operand(word)),
+        "psw"  => Some(Token::Operand(word)),
+        "sp"   => Some(Token::Operand(word)),
+        "0"    => Some(Token::Operand(word)),
+        "1"    => Some(Token::Operand(word)),
+        "2"    => Some(Token::Operand(word)),
+        "3"    => Some(Token::Operand(word)),
+        "4"    => Some(Token::Operand(word)),
+        "5"    => Some(Token::Operand(word)),
+        "6"    => Some(Token::Operand(word)),
+        "7"    => Some(Token::Operand(word)),
+        _      => Some(Token::Symbol(word)),
+    }
 }
--- src/bin.rs
+++ src/bin.rs
@@ -3,7 +3,7 @@ use pp8085_lib::cpu8085::PP8085;
 use pp8085_lib::asm8085::*;
 
 fn main() {
-    let (bin1, listing1) = parse("examples/test.asm").unwrap();
+    let (bin1, listing1) = parse("examples/led1.asm").unwrap();
     println!("{}", listing1);
     let rom = Memory::new_from(&bin1, 1024*8);
     let mut cpu = PP8085::new();
